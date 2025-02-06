7a3c46e45687b526751107cd030801a1629b27ab
lopesma11 <ml722090@gmail.com>
2025-02-06 11:00:14 -0300

    task: dealing with Mocks

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
+import { userRepositoryMock } from '../testing/user-repository-mock';
 
 describe('UserService', () => {
   let userService: UserService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, userRepositoryMock],
     }).compile();
 
     userService = module.get<UserService>(UserService);
